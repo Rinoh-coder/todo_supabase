17500264e9143c750218450fac53768795461256
Rinoh-coder <9965rinoh@gmail.com>
2026-04-15 00:57:53 +0200

    test de la connexion avec import et ajout de console.log

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,8 +1,10 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
+import supabase from "@/supabase/supabaseClient";
 
 const Layout = () => {
+  console.log(supabase);
   return (
     <>
       <Header />
