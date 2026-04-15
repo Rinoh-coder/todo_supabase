9ec5dbe6bfa1d21fe14b9d5a29a9fbef1450ee9b
rinoh <9965rinoh@gmail.com>
2026-04-15 15:26:54 +0300

    nettoyage et structuration des codes, suppression des commentaires de codes inutiles et réorganisation des codes

--- src/components/ListsDesTaches.jsx
+++ src/components/ListsDesTaches.jsx
@@ -4,25 +4,6 @@ import supabase from "@/supabase/supabaseClient";
 
 const ListeDesTaches = () => {
   const [ taches, setTaches ] = useState([]);
-  // const taches = [
-  //   {
-  //     id: 1,
-  //     titre: "Créer une vidéo marketing",
-  //     description:
-  //       "Développer une vidéo promotionnelle pour le lancement du nouveau produit, en mettant l'accent sur les principales caractéristiques et les avantages pour les clients.",
-  //     status: false,
-  //     created_at: "2023-08-15T10:30:00Z",
-  //   },
-  //   {
-  //     id: 2,
-  //     titre: "Mettre à jour le contenu du site web",
-  //     description:
-  //       "Réviser le contenu de la page d'accueil pour refléter les récents changements dans la gamme de produits et améliorer le SEO.",
-  //     status: false,
-  //     created_at: "2023-08-20T09:00:00Z",
-  //   },
-  // ];
-useEffect(() => {
   const fetchTaches = async () => {
     const { data, error } = await supabase.from("taches").select("*").order("id", { ascending: false });
     if (error) {
@@ -31,6 +12,8 @@ useEffect(() => {
       setTaches(data);
     }
   };
+useEffect(() => {
+
   fetchTaches();
 }, []);
   return (
